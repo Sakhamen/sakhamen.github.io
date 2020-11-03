7badc8402254ea814e5b5ba70c33f07adaf74d6b
sakhamen <sakha@bcom.co.za>
2020-11-03 08:29:49 +0200

    Page Loader using Pace JS.

--- forms/contact.php
+++ forms/contact.php
@@ -22,15 +22,14 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  // Gmail SMTP
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.gmail.com',
+    'username' => 'dopestsakhamen@gmail.com',
+    'password' => 'Sakha123!',
     'port' => '587'
   );
-  */
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
